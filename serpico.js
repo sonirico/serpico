69fe35d0ffec90f4c7c891aa462e505f007370d1
sonirico <marsanben92@hotmail.com>
2017-05-10 23:08:32 +0200

    Feature add sort and filter operations

--- serpico.js
+++ serpico.js
@@ -1,9 +1,11 @@
-(function ($) {
+function oddOrNot (n) {
+    return n % 2 == 0 ? 'even' : 'odd';
+};
 
+(function ($) {
 
     $.fn.serpico = function (options) {
         var opts = $.extend({}, $.fn.serpico.defaults, options);
-console.log(opts);
         var galleries = [];
 
         this.each(function () {
@@ -19,25 +21,38 @@ console.log(opts);
         namespace: 'serpico',
         columns: 3,
         data: [
-          {src: 'img1', sort: 1},
-          {src: 'img2', sort: 3},
-          {src: 'img3', sort: 2},
+          {src: 'img1', sort: 1, filter: oddOrNot(1)},
+          {src: 'img2', sort: 3, filter: oddOrNot(3)},
+          {src: 'img3', sort: 2, filter: oddOrNot(2)},
         ],
-        minHeight: '100px'
+        minHeight: '100px',
+        sortField: 'sort',
+        filterAll: '_all',
+        filterAttribute: 'filter',//TODO: filter function as parameters
+        filterSeparator: " ",
+        onFilterGlobal: function (target) {
+
+        },
+        onFilterItem: function (item) {
+
+        },
+        onSortGlobal: function (target) {
+
+        },
+        onSortItem: function () {
+
+        }
     };
 
     function Gallery (target, config) {
         var me = this;
 
-        me.init = function () {
-            target.addClass(config.namespace);
-            target.css({
-              'min-height': config.minHeight
-            });
-
+        // Private functions
+        var append = function (data) {
             var w = (100 / config.columns) + '%';
 
-            config.data.forEach(function (item, index) {
+            data.forEach(function (item, index) {
+              
                 target.append(
                     $('<div>')
                       .addClass('item')
@@ -45,23 +60,58 @@ console.log(opts);
                           'width': w
                       })
                       .data(item)
-                      .text(item.src)
+                      .html(
+                          JSON.stringify(item, null, 2)
+                      )
                 );
             });
+        }
+
+        me.init = function () {
+            target.addClass(config.namespace);
+            target.css({
+              'min-height': config.minHeight
+            });
+
+            append(config.data);
+        };
+
+        me.add = function (newData) {
+            append(newData);
+        };
+
+        me.filter = function (keyword) {
+            if (keyword === config.filterAll) {
+
+                target.find('.item').show();
+
+            } else {
+
+                target.find('.item').each(function () {
+                    var filters = $(this).data('filter')
+                        .trim()
+                        .split(config.filterSeparator);
+
+                    if (keyword.indexOf(filters) < 0) {
+                        $(this).hide();
+                    } else {
+                        $(this).show();
+                    }
+                });
+
+            }
+        }
+
+        me.sort = function (dataAttribute) {
+            target.find('.item').sort(function (a, b) {
+
+                return $(a).data(config.sortField) - $(b).data(config.sortField);
+
+            }).appendTo(target);
         };
 
         me.init();
     }
-
-    Gallery.prototype.add = function (moreData) {
-      console.log(this);
-console.log(moreData);
-    };
-
-    Gallery.prototype.sort = function () {
-
-    };
-
 
 
 })(jQuery);
